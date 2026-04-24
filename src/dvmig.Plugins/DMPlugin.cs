671f0c58f528a4e2f32968f451b7fd40e04c933c
Daniel Solin <daniel@solin.org>
2026-04-24 13:48:28 +0200

    refactor: split DMPlugin logic and add XML documentation

--- src/dvmig.Plugins/DMPlugin.cs
+++ src/dvmig.Plugins/DMPlugin.cs
@@ -4,9 +4,21 @@ using Microsoft.Xrm.Sdk.Query;
 
 namespace dvmig.Plugins
 {
-    // PreOperation - Synchronous - Create/Update of Any
+    /// <summary>
+    /// Data Migration Plugin responsible for preserving source environment 
+    /// timestamps (CreatedOn and ModifiedOn) during the migration process.
+    /// It works by looking up temporary date records in the dm_sourcedate 
+    /// entity and applying them to the target entity before it is saved.
+    /// </summary>
     public class DMPlugin : IPlugin
     {
+        /// <summary>
+        /// Main entry point for the plugin execution.
+        /// </summary>
+        /// <param name="serviceProvider">
+        /// The service provider that contains the execution context, 
+        /// tracing service, and organization service factory.
+        /// </param>
         public void Execute(IServiceProvider serviceProvider)
         {
             var context = (IPluginExecutionContext)serviceProvider
@@ -37,65 +49,131 @@ namespace dvmig.Plugins
                 return;
             }
 
-            var messageName = context.MessageName.ToLower();
-
             try
             {
-                var fetchXml = $@"
-                    <fetch version='1.0' output-format='xml-platform' 
-                           mapping='logical' distinct='false' count='1'>
-                      <entity name='dm_sourcedate'>
-                        <attribute name='dm_sourcedateid' />
-                        <attribute name='dm_sourcecreateddate' />
-                        <attribute name='dm_sourcemodifieddate' />
-                        <filter type='and'>
-                          <condition attribute='dm_sourceentityid' 
-                            operator='eq' value='{entity.Id}' />
-                          <condition attribute='dm_sourceentitylogicalname' 
-                            operator='eq' 
-                            value='{entity.LogicalName.ToLower()}' />
-                        </filter>
-                      </entity>
-                    </fetch>";
-
-                var result = service.RetrieveMultiple(
-                    new FetchExpression(fetchXml)
+                HandleDatePreservation(
+                    context,
+                    service,
+                    tracingService,
+                    entity
                 );
-
-                if (result.Entities.Count > 0)
-                {
-                    var sourceDate = result.Entities[0];
-
-                    if (messageName == "create")
-                    {
-                        if (sourceDate.Contains("dm_sourcecreateddate"))
-                        {
-                            var createdDate =
-                                sourceDate["dm_sourcecreateddate"];
-
-                            entity["createdon"] = createdDate;
-                            entity["overriddencreatedon"] = createdDate;
-                        }
-
-                        if (sourceDate.Contains("dm_sourcemodifieddate"))
-                        {
-                            entity["modifiedon"] =
-                                sourceDate["dm_sourcemodifieddate"];
-                        }
-                    }
-                    else if (messageName == "update")
-                    {
-                        if (sourceDate.Contains("dm_sourcemodifieddate"))
-                        {
-                            entity["modifiedon"] =
-                                sourceDate["dm_sourcemodifieddate"];
-                        }
-                    }
-                }
             }
             catch (Exception ex)
             {
-                tracingService.Trace("DMPlugin error: {0}", ex.ToString());
+                tracingService.Trace(
+                    "DMPlugin error: {0}",
+                    ex.ToString()
+                );
+            }
+        }
+
+        /// <summary>
+        /// Orchestrates the date preservation logic by identifying the 
+        /// message type and applying the appropriate source dates.
+        /// </summary>
+        private void HandleDatePreservation(
+            IPluginExecutionContext context,
+            IOrganizationService service,
+            ITracingService tracingService,
+            Entity entity
+        )
+        {
+            var messageName = context.MessageName.ToLower();
+            var sourceDate = GetSourceDate(
+                service,
+                entity.Id,
+                entity.LogicalName
+            );
+
+            if (sourceDate == null)
+            {
+                return;
+            }
+
+            if (messageName == "create")
+            {
+                ApplyCreateDates(entity, sourceDate);
+            }
+            else if (messageName == "update")
+            {
+                ApplyUpdateDates(entity, sourceDate);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the source date record from the dm_sourcedate entity 
+        /// for the specified target entity.
+        /// </summary>
+        private Entity? GetSourceDate(
+            IOrganizationService service,
+            Guid entityId,
+            string logicalName
+        )
+        {
+            var fetchXml = $@"
+                <fetch version='1.0' output-format='xml-platform' 
+                       mapping='logical' distinct='false' count='1'>
+                  <entity name='dm_sourcedate'>
+                    <attribute name='dm_sourcedateid' />
+                    <attribute name='dm_sourcecreateddate' />
+                    <attribute name='dm_sourcemodifieddate' />
+                    <filter type='and'>
+                      <condition attribute='dm_sourceentityid' 
+                        operator='eq' value='{entityId}' />
+                      <condition attribute='dm_sourceentitylogicalname' 
+                        operator='eq' 
+                        value='{logicalName.ToLower()}' />
+                    </filter>
+                  </entity>
+                </fetch>";
+
+            var result = service.RetrieveMultiple(
+                new FetchExpression(fetchXml)
+            );
+
+            if (result.Entities.Count > 0)
+            {
+                return result.Entities[0];
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Applies both CreatedOn and ModifiedOn dates from the source 
+        /// record to the target entity during a Create operation.
+        /// </summary>
+        private void ApplyCreateDates(
+            Entity entity,
+            Entity sourceDate
+        )
+        {
+            if (sourceDate.Contains("dm_sourcecreateddate"))
+            {
+                var createdDate = sourceDate["dm_sourcecreateddate"];
+
+                entity["createdon"] = createdDate;
+                entity["overriddencreatedon"] = createdDate;
+            }
+
+            if (sourceDate.Contains("dm_sourcemodifieddate"))
+            {
+                entity["modifiedon"] = sourceDate["dm_sourcemodifieddate"];
+            }
+        }
+
+        /// <summary>
+        /// Applies the ModifiedOn date from the source record to the 
+        /// target entity during an Update operation.
+        /// </summary>
+        private void ApplyUpdateDates(
+            Entity entity,
+            Entity sourceDate
+        )
+        {
+            if (sourceDate.Contains("dm_sourcemodifieddate"))
+            {
+                entity["modifiedon"] = sourceDate["dm_sourcemodifieddate"];
             }
         }
     }
