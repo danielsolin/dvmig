307fe883840c9081ee8873c99b0ce466f1eba45d
Daniel Solin <daniel@solin.org>
2026-05-13 12:13:31 +0200

    Revert "Removed #pragma"
    This reverts commit 5943b4a7c452f2b339d85a9bf223f0d3312bb95a.

--- src/dvmig.Core/Shared/Logger.cs
+++ src/dvmig.Core/Shared/Logger.cs
@@ -1,3 +1,6 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary.
+using System;
+#pragma warning restore IDE0005 // Using directive is unnecessary.
 using System.Text.RegularExpressions;
 using Serilog;
 using SerilogLogger = Serilog.ILogger;
